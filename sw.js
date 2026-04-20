64678e3843da52815f36354015d72991283476f7
Kalani <kalani@Kalanis-MacBook-Air.local>
2026-04-19 22:40:12 -0400

    init

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE = 'kt-v1';
+var FILES = ['/'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      var network = fetch(e.request).then(function(res) {
+        if(res&&res.status===200) {
+          var clone = res.clone();
+          caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+        }
+        return res;
+      }).catch(function(){return cached;});
+      return cached || network;
+    })
+  );
+});
