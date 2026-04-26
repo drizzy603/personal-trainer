4188e1d18640a93cb9baac62f3c0077c691d15a3
Roberto <drizzy.603@gmail.com>
2026-04-26 00:33:21 -0400

    Use app-icon-1024.png as PWA and Apple touch icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE = 'kt-v8';
-var FILES = ['./', './sw.js', './manifest.json'];
+var CACHE = 'kt-v9';
+var FILES = ['./', './sw.js', './manifest.json', './app-icon-1024.png'];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
